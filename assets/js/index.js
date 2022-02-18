92ba3b577acb01260e02edc9492b7c346954750a
Nouira Alim <nouiraalim@gmail.com>
2022-02-18 09:03:37 +0100

    Correction de la sélection des étiquettes
    Sélection de la première étiquette maintenant possible

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,12 @@ let url;
 let selectedTags = [];
 
 document.querySelector("select#tags").addEventListener("change", addTag);
+document.querySelector("select#tags").addEventListener("focus", () =>
+{
+    document.querySelector("select#tags").value=-1;
+    document.querySelector("select#tags").blur();
+});
+
 document.querySelector("button.confirm").addEventListener("click", addBookmark);
 
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
