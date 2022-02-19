5c3207d68bf792922119e8496baaf04d8d3f302d
Nouira Alim <nouiraalim@gmail.com>
2022-02-19 15:48:39 +0100

    Changement de la structure pour stocker les données
    Réécriture de l'écriture et de l'accès aux
    données enregistrées sur le navigateur

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 let url;
 let selectedTags = [];
 
-document.querySelector("select#tags").addEventListener("change", addTag);
+document.querySelector("select#tags").addEventListener("change", function(){addTag(this)});
 document.querySelector("select#tags").addEventListener("focus", () =>
 {
     document.querySelector("select#tags").value=-1;
@@ -11,21 +11,8 @@ document.querySelector("select#tags").addEventListener("focus", () =>
 document.querySelector("button.confirm").addEventListener("click", addBookmark);
 
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-
-    // let value = JSON.stringify({"url": tabs[0].url, "title": tabs[0].title});
-    // localStorage.setItem("greetings", value);
-
-    // let results = localStorage.getItem("greetings");
-    // document.querySelector("input#name").value = JSON.parse(results).title;
-    // document.querySelector(".test").innerText = JSON.parse(results).url;
-
-    // localStorage.setItem("tags", JSON.stringify([{"C": 0}, {"C++": 0}, {"Design": 0}]));
-
-    getTagsAsOptions();
-
     document.querySelector("input#name").value = tabs[0].title;
     url = tabs[0].url;
 
-    // Affecter les valeurs au moment de la validation, pas avant
-
+    getTagsAsOptions();
 });
--- assets/js/tags.js
+++ assets/js/tags.js
@@ -2,66 +2,68 @@ function getTagsAsButtons()
 {
     const tagsContainer = document.querySelector("aside.tagsMain");
 
-    const tags = JSON.parse(localStorage.getItem("tags"));
+    const tagsList = JSON.parse(localStorage.getItem("tagsList"));
 
-    for (let cnt = 0; cnt < tags.length; cnt++) {
+    for (const key of Object.keys(tagsList)) 
+    {
         const tag = document.createElement("div");
         tag.className = "tag";
-        tag.innerText = Object.keys(tags[cnt])[0];
+        tag.innerText = key;
 
-        tag.addEventListener("click", () => {
+        tag.addEventListener("click", () => 
+        {
             this.classList.toggle("selected");
         })
 
-        tagsContainer.appendChild(tag);    
-    }
+        tagsContainer.appendChild(tag);  
+   }
 }
 
 function getTagsAsOptions()
 {
     let tagsSelect = document.querySelector("select#tags");
-    const tags = JSON.parse(localStorage.getItem("tags"));
+    const tagsList = JSON.parse(localStorage.getItem("tagsList"));
 
-    for (let cnt = 0; cnt < tags.length; cnt++) 
+    // unselectedTags = tagsList.filter(function(selectedTags) { return selectedTags.includes(Object.keys(tagsList[tagId])[0]) })
+
+    for (const [key, value] of Object.entries(tagsList)) 
     {
         const option = document.createElement("option");
-        option.value =  cnt;
-        option.innerText = Object.keys(tags[cnt])[0];
+        option.value =  value.id;
+        option.innerText = key;
         tagsSelect.appendChild(option);
     }
 }
 
-function addTag()
+function addTag(select)
 {
-    const tagValue = parseInt(document.querySelector('select#tags').value);
+    const selectedOption = document.querySelector("option[value='" + select.value + "']");
     
-    selectedTags.push(tagValue);
+    selectedTags.push([selectedOption.value, selectedOption.innerText]);
 
     const tagsContainer = document.querySelector("div.tagsMain");
 
-    document.querySelector('select#tags').removeChild(document.querySelector("option[value='" + tagValue + "']"));
-
-    const tags = JSON.parse(localStorage.getItem("tags"));
-
     const tag = document.createElement("div");
     tag.className = "tag";
-    tag.innerText = Object.keys(tags[tagValue])[0];
+    tag.innerText = selectedOption.innerText;
 
     tag.appendChild(createCross());
 
     tagsContainer.appendChild(tag);
+
+    select.removeChild(document.querySelector("option[value='" + selectedOption.value + "']"));
 }
 
 function removeTag()
 {
-    
+
 }
 
 function createCross()
 {
     const crossButton = document.createElement("div");
     crossButton.className = "cross-button";
-    crossButton.addEventListener("click", removeTag())
+    crossButton.addEventListener("click", function(){ removeTag(); });
     
     const crossTop = document.createElement("div");
     crossTop.className = "cross-top";    
