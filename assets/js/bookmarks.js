3b460bfc88d52ceaa5e36b61debffe0e88f154aa
Nouira Alim <nouiraalim@gmail.com>
2022-02-20 08:06:11 +0100

    Ajout de getSelectedTagsID() Retourne les tags sélectionnés pour le favori en cours d'ajout

--- assets/js/bookmarks.js
+++ assets/js/bookmarks.js
@@ -4,13 +4,25 @@ function addBookmark()
 
     const bookmark = 
     {
-        title:document.querySelector("input#name").value,
-        url: url,        
-        tags: selectedTags.values
+        "title": document.querySelector("input#name").value,
+        "url": url,        
+        "tags": getSelectedTagsID()
     };
 
     bookmarks.push(bookmark);
 
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 
+}
+
+function getSelectedTagsID()
+{
+    tagsID = [];
+
+    for ([tagID, ] of selectedTags)
+    {
+        tagsID.push(tagID);
+    }
+
+    return tagsID;
 }
