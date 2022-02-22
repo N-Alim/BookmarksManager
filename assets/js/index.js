78199becf3332533cd91c11b357167e061832eb4
Nouira Alim <nouiraalim@gmail.com>
2022-02-23 00:29:51 +0100

    Initialisation des données à l'installation
    Création des clés et valeurs utilisées dans l'extension
    dans localStorage à l'installation grâce à une vérification de
    la version de l'extension stocké dans localStorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+checkVersionChanges()
+
 let url;
 let selectedTags = [];
 
---
+++ config.js
@@ -0,0 +1,28 @@
+function checkVersionChanges()
+{
+    const currVersion = getVersion();
+    const prevVersion = localStorage['version']
+
+    if (currVersion != prevVersion) 
+    {
+        if (typeof prevVersion == 'undefined') 
+            onInstall();
+
+        // else 
+        //     onUpdate();
+
+        localStorage['version'] = currVersion;
+    }
+}
+
+function getVersion() 
+{
+    return chrome.app.getDetails().version;
+}
+
+function onInstall() 
+{
+    localStorage["bookmarks"] = JSON.stringify({});
+    localStorage["tagsList"] = JSON.stringify({"Undefined": {id: 0, useCount: 0}});
+    localStorage["lastTagIDUsed"] = JSON.stringify(0);  
+}
